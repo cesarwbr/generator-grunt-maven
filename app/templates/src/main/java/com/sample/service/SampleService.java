20e8354940006ec3ee649105680a77bb7ca7ba25
Cesar William <cesarwbr@gmail.com>
2014-01-26 17:59:04 -0200

    add a backend service

---
+++ app/templates/src/main/java/com/sample/service/SampleService.java
@@ -0,0 +1,79 @@
+package com.sample.service;
+
+import com.sample.view.SampleView;
+import es.cenobit.struts2.json.annotations.Json;
+import org.apache.struts2.ServletActionContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by cesar on 1/26/14.
+ */
+public class SampleService {
+    private String name;
+    private String description;
+
+    @Json
+    public Object generic() {
+
+        String method = ServletActionContext.getRequest().getMethod();
+        if ( method.equals( "GET" ) ) {
+            return findAllSample();
+        }
+        else if ( method.equals( "POST" ) ) {
+            return saveSample();
+        }
+        return null;
+    }
+
+    private SampleView saveSample() {
+        SampleView sampleView = new SampleView();
+        sampleView.setName(this.name);
+        sampleView.setDescription(this.description);
+
+        // TODO save object
+
+        return sampleView;
+    }
+
+    private List<SampleView> findAllSample(){
+
+        // TODO call a service
+
+        List<SampleView> sampleViews = new ArrayList<SampleView>();
+
+        SampleView sampleView = new SampleView();
+        sampleView.setName("Test 1");
+        sampleView.setDescription("Description 1");
+        sampleViews.add(sampleView);
+
+        sampleView = new SampleView();
+        sampleView.setName("Test 2");
+        sampleView.setDescription("Description 2");
+        sampleViews.add(sampleView);
+
+        sampleView = new SampleView();
+        sampleView.setName("Test 3");
+        sampleView.setDescription("Description 3");
+        sampleViews.add(sampleView);
+
+        return sampleViews;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ app/templates/src/main/java/com/sample/view/SampleView.java
@@ -0,0 +1,25 @@
+package com.sample.view;
+
+/**
+ * Created by cesar on 1/26/14.
+ */
+public class SampleView {
+    private String name;
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
